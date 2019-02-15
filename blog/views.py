ad9ba43a98a7ec9b0e2f6968cb6381459dc37153
marek357 <marekmmasiak@gmail.com>
2019-02-15 18:24:32 +0100

    bug fix

--- blog/views.py
+++ blog/views.py
@@ -63,58 +63,58 @@ def wordstats(request):
     except FileNotFoundError:
         context = {'dirname': directory, 'tytul': 'Statystyki wiadomości', 'error': 'Niestety w podanej ścieżce nie znaleziono plików z danymi Facebooka'}
 
-        for i in range(0, len(messageData)):
-            if messageData[i] != '.DS_Store':
-                try:
-                    messagePaths.append(messageDataDirectory + '/' + messageData[i] + '/message.json')
-                except:
-                    pass
+    for i in range(0, len(messageData)):
+        if messageData[i] != '.DS_Store':
+            try:
+                messagePaths.append(messageDataDirectory + '/' + messageData[i] + '/message.json')
+            except:
+                pass
 
-        """
-        Kod zbiera zawartosci poszczegolnych plikow
-        zawierajace wiadomosci
-        """
-        messageContent = []
-        words = {}
+    """
+    Kod zbiera zawartosci poszczegolnych plikow
+    zawierajace wiadomosci
+    """
+    messageContent = []
+    words = {}
 
-        for i in messagePaths:
-            temporaryFile = open(i, 'r')
-            if temporaryFile.mode == 'r':
-                try:
-                    tempContainer = json.loads(temporaryFile.read())
-                    messageContent.append(json.loads(json.dumps(tempContainer).encode('latin1').decode('utf-8')))
-                except:
-                    pass
-            temporaryFile.close()
+    for i in messagePaths:
+        temporaryFile = open(i, 'r')
+        if temporaryFile.mode == 'r':
+            try:
+                tempContainer = json.loads(temporaryFile.read())
+                messageContent.append(json.loads(json.dumps(tempContainer).encode('latin1').decode('utf-8')))
+            except:
+                pass
+        temporaryFile.close()
 
-        """
-        Kod robi statystyki 100 najczesciej uzywanych slow
-        """
+    """
+    Kod robi statystyki 100 najczesciej uzywanych slow
+    """
 
-        for i in range(0, len(messageContent)):
-            for a in messageContent[i]['messages']:
-                a['content'] = str(a['content']).encode('latin1').decode('utf-8', 'ignore')
-                for x in word_tokenize(a['content']):
-                    if x not in words:
-                        words[x] = 1
-                    else:
-                        words[x] += 1
+    for i in range(0, len(messageContent)):
+        for a in messageContent[i]['messages']:
+            a['content'] = str(a['content']).encode('latin1').decode('utf-8', 'ignore')
+            for x in word_tokenize(a['content']):
+                if x not in words:
+                    words[x] = 1
+                else:
+                    words[x] += 1
 
-        words = sorted(words.items(), key=operator.itemgetter(1), reverse=True)
-        wordList = []
-        wordDict = {}
-        wordKeys = []
-        wordItems = []
-        for i in range(0, 50):
-            wordList.append(words[i][0] + ' (' + str(words[i][1]) + ')')
-            wordDict[words[i][0]] = words[i][1]
-            wordKeys.append(words[i][0])
-            wordItems.append(words[i][1])
-        # plt.plot(wordKeys,wordItems)
-        # plt.xlabel('Słowo')
-        # plt.ylabel('Liczba wystąpień')
-        # plt.savefig('/stat.png')
-        context = {'tytul': 'Strona domowa', 'words': wordList, 'data': wordDict, 'pic': '/stat.png'}
+    words = sorted(words.items(), key=operator.itemgetter(1), reverse=True)
+    wordList = []
+    wordDict = {}
+    wordKeys = []
+    wordItems = []
+    for i in range(0, 50):
+        wordList.append(words[i][0] + ' (' + str(words[i][1]) + ')')
+        wordDict[words[i][0]] = words[i][1]
+        wordKeys.append(words[i][0])
+        wordItems.append(words[i][1])
+    # plt.plot(wordKeys,wordItems)
+    # plt.xlabel('Słowo')
+    # plt.ylabel('Liczba wystąpień')
+    # plt.savefig('/stat.png')
+    context = {'tytul': 'Strona domowa', 'words': wordList, 'data': wordDict, 'pic': '/stat.png'}
 
     return render(request, 'blog/wordstats.html', context)
 
@@ -135,59 +135,59 @@ def messagestats(request):
     except FileNotFoundError:
         context = {'dirname': directory, 'tytul': 'Reklamodawcy', 'error': 'Niestety w podanej ścieżce nie znaleziono plików z danymi Facebooka'}
 
-        for i in range(0, len(messageData)):
-            if messageData[i] != '.DS_Store':
-                try:
-                    messagePaths.append(messageDataDirectory + '/' + messageData[i] + '/message.json')
-                except:
-                    pass
-        """
-        Kod zbiera zawartosci poszczegolnych plikow
-        zawierajace wiadomosci
-        """
-        messageContent = []
-        messageCount = {}
-        for i in messagePaths:
-            temporaryFile = open(i, 'r')
-            if temporaryFile.mode == 'r':
-                try:
-                    messageContent.append(json.loads(temporaryFile.read()))
-                except:
-                    pass
+    for i in range(0, len(messageData)):
+        if messageData[i] != '.DS_Store':
+            try:
+                messagePaths.append(messageDataDirectory + '/' + messageData[i] + '/message.json')
+            except:
+                pass
+    """
+    Kod zbiera zawartosci poszczegolnych plikow
+    zawierajace wiadomosci
+    """
+    messageContent = []
+    messageCount = {}
+    for i in messagePaths:
+        temporaryFile = open(i, 'r')
+        if temporaryFile.mode == 'r':
+            try:
+                messageContent.append(json.loads(temporaryFile.read()))
+            except:
+                pass
 
-            temporaryFile.close()
+        temporaryFile.close()
 
-        """
-        ZADANIE 1
-        Kod liczy ilosc rzeczy z JSONArray messages
-        i srednia ilosc wiadomosci
-        """
-        averageNumberOfMessages = 0
+    """
+    ZADANIE 1
+    Kod liczy ilosc rzeczy z JSONArray messages
+    i srednia ilosc wiadomosci
+    """
+    averageNumberOfMessages = 0
 
-        for i in range(0, len(messageContent)):
-            messageCount[i] = len(messageContent[i]['messages'])
-            averageNumberOfMessages += messageCount[i]
-        messageCount = sorted(messageCount.items(), key=operator.itemgetter(1), reverse=True)
+    for i in range(0, len(messageContent)):
+        messageCount[i] = len(messageContent[i]['messages'])
+        averageNumberOfMessages += messageCount[i]
+    messageCount = sorted(messageCount.items(), key=operator.itemgetter(1), reverse=True)
 
-        averageNumberOfMessages /= len(messageContent)
+    averageNumberOfMessages /= len(messageContent)
 
-        """
-        Wypisuje top 5 osob,
-        z ktorymi pisales
-        """
+    """
+    Wypisuje top 5 osob,
+    z ktorymi pisales
+    """
 
-        #print('5 osob, z ktorymi najczesciej pisales to:')
+    #print('5 osob, z ktorymi najczesciej pisales to:')
 
-        numberOfGroupChats = 1
-        # print(len(messageCount))
-        ppl = []
-        for i in range(0, len(messageCount)):
-            if len(messageContent[messageCount[i][0]]['participants']) <= 2:
-                #print(str(messageContent[messageCount[i][0]]['participants'][0]['name']) + '(' + str(messageCount[i][1])+')')
-                ppl.append(str(messageContent[messageCount[i][0]]['participants'][0]['name']) + '(' + str(messageCount[i][1]) + ')')
-            else:
-                numberOfGroupChats += 1
-        context = {'dirname': directory, 'tytul': 'Strona Domowa', 'range': range(13), 'nr': numberOfGroupChats, 'avgmsg': averageNumberOfMessages, 'ppl': ppl}
+    numberOfGroupChats = 1
+    # print(len(messageCount))
+    ppl = []
+    for i in range(0, len(messageCount)):
+        if len(messageContent[messageCount[i][0]]['participants']) <= 2:
+            #print(str(messageContent[messageCount[i][0]]['participants'][0]['name']) + '(' + str(messageCount[i][1])+')')
+            ppl.append(str(messageContent[messageCount[i][0]]['participants'][0]['name']) + '(' + str(messageCount[i][1]) + ')')
+        else:
+            numberOfGroupChats += 1
+    context = {'dirname': directory, 'tytul': 'Strona Domowa', 'range': range(13), 'nr': numberOfGroupChats, 'avgmsg': averageNumberOfMessages, 'ppl': ppl}
 
     return render(request, 'blog/messagestats.html', context)
 
